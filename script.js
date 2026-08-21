8c4b6f7fb0b93a6ef5d05cd54d43fadfd474d5f4
BrandonDono <al222310551@gmail.com>
2026-08-21 05:48:37 -0600

    style: se ha agregado la funcion de CalPrestamo

---
+++ script.js
@@ -0,0 +1,32 @@
+
+async function CalPrestamo()
+{
+const mon =Number(document.getElementById("monto").value);
+const tas = Number(document.getElementById("tasa").value)
+const plaz = Number(document.getElementById("plazo").value)
+const iva =Number(document.getElementById("iva").value)
+
+
+if(mon <= 0 || tas <0 || plaz <=0 || iva <0 ){
+alert("Ingresa los datos correspondientes")
+return;
+}
+
+const tamensual =(tas/100)/12;
+let mensualidad;
+
+if(tamensual===0){
+    mensualidad = mon/plaz;
+}
+else{
+    mensualidad = mon * (tamensual * Math.pow(1+tamensual,plaz))/(Math.pow(1+tamensual,plaz)-1);
+}
+const interes = mensualidad * plaz - mon;
+const ivainteres = interes *(iva/100);
+const  total = mon + interes + ivainteres;
+
+  
+}
+
+
+
